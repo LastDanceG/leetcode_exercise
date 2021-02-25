d8e4171fdc505d56b83ebeaa8519916a568d2624
LastDanceG <L5623064@163.com>
2021-02-25 18:22:23 +0800

    利用pandas将数据导入csv文件

--- pandas/exercise.py
+++ pandas/exercise.py
@@ -4,6 +4,14 @@
 import pandas as pd
 
 
+def import_csv(path, data, headers):
+    """
+    import csv file
+    """
+    df = pd.DataFrame(data)
+    df.to_csv(path, mode='w', header=headers, index=False)
+
+
 if __name__ == '__main__':
     data = [
         ['2021-02-01', 'donny', '23', 'login'],
@@ -11,6 +19,10 @@ if __name__ == '__main__':
         ['2021-02-01', 'donny', '23', 'login_out'],
     ]
     headers = ['date', 'name', 'age', 'action']
-    data_frame = pd.DataFrame(data, columns=headers)
+    #
+    # data_frame = pd.DataFrame(data, columns=headers)
+    #
+    # print data_frame.to_json(orient='index')
 
-    print data_frame.to_json(orient='index')
+    path = './test.csv'
+    import_csv(path, data, headers)
